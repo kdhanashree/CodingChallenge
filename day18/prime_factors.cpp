6c02a002de64af4c317547b75366d7ed835bc484
Dhanashree Kesarkar <kesarkardhanu@gmail.com>
2024-09-25 22:02:10 +0530

    submission

---
+++ day18/prime_factors.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> prime_factors(int n){
+    vector<int> ans;
+    while(n!=0 && n%2 == 0){
+        n /=2;
+        ans.push_back(2);
+    }
+
+    for(int i=3;i*i<=n;i++)
+    {
+        while(n%i == 0){
+            n/=i;
+            ans.push_back(i);
+        }
+    }
+
+    if(n>2){
+        ans.push_back(n);
+    }
+    return ans;
+}
+int main(){
+    cout<<"Enter  a number:";
+    int n;
+    cin>>n;
+
+    vector<int> ans = prime_factors(n);
+
+    cout<<"Prime Factors:";
+    for(auto x: ans)
+    {
+        cout<<x<<" ";
+    }
+
+}
+//Time-Complexity:O(sqrt(n)).
